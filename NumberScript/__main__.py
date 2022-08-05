712a2a0a2384afb5ba4029c903a51b1f85ded826
Sasen Perera <sasen.learnings@gmail.com>
2022-08-05 16:55:31 +0530

    Made this into a PYPI Package

---
+++ NumberScript/__main__.py
@@ -0,0 +1,42 @@
+from interpreter import Interpreter
+import argparse
+import os
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--file", help="file to interpret")
+
+args = parser.parse_args()
+run = True
+
+space = ""
+for j in range(0, 16):
+    space += " "
+
+for k in range(0, len(os.name)):
+    space = space.replace(" ", "", 1)
+
+if args.file:
+    with open(args.file, "r") as file:
+        code = file.read()
+        Interpreter.interpret(code)
+else:
+    print(f"""
+        +------------------------------------------+
+        | NumberScript Shell: Type 'exit' to exit. |
+        | Version: 1.0 Machine: {os.name} {space}  |
+        | Type 'help' for more information.        |
+        | {current_time}                                 |
+        +------------------------------------------+
+    """)
+    while run:
+        code = input(">")
+        if code == "exit":
+            run = False
+        else:
+            Interpreter.interpret(code)
---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open('README.md') as f:
+    longest_description = f.read()
+
+setuptools.setup(
+    name="NumberScript",
+    version="1.5.5",
+    description="possibly the world's most simplest and restricted language.",
+    author="Sasen Perera",
+    long_description=longest_description,
+    long_description_content_type="text/markdown",
+    packages=["NumberScript"],
+)
