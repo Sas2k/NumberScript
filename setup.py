17ecb75325879745b3d08eb5d037f0a3b56ac2f1
Sasen Perera <sasen.learnings@gmail.com>
2022-08-10 18:37:36 +0530

    Syntax changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name="NumberScript",
-    version="1.11.5",
+    version="1.11.6",
     description="possibly the world's most simplest and restricted language.",
     author="Sasen Perera",
     long_description=longest_description,
